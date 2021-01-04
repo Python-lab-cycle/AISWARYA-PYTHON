e2592d7f40837dd0e3aaf44b64832107e6061c25
Aiswarya-github <75841804+Aiswarya-github@users.noreply.github.com>
2021-01-04 13:57:54 +0530

    Update list_of_vowels.py

--- list_of_vowels.py
+++ list_of_vowels.py
@@ -1,8 +1,8 @@
-word = input("Enter a word: ")
-for letter in word:
-    if letter in 'aeiou':
-        print(letter, end="")
-
 # word = input("Enter a word: ")
-# vowels = [i for i in word if i in 'aeiou']
-# print(vowels)
+# for letter in word:
+#     if letter in 'aeiou':
+#         print(letter, end="")
+
+word = input("Enter a word: ")
+vowels = [i for i in word if i in 'aeiou']
+print(vowels)

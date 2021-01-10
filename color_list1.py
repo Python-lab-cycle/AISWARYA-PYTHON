d11205ea2b151a22f6f04101a91b97e01a62176a
Aiswarya-github <75841804+Aiswarya-github@users.noreply.github.com>
2021-01-10 21:10:42 +0530

    Add files via upload

---
+++ color_list1.py
@@ -0,0 +1,5 @@
+color = input("Enter colors(space between colors): ")
+color_list = color.split(" ")
+print(color_list)
+print("The first color in the list is :", color_list[0], "And the last color in the list is :", color_list[-1])
+
